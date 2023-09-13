3546df47619f742084775deb445367eccc48a0e2
Rahul Kamilya <68813555+rahulkamilya@users.noreply.github.com>
2023-09-13 15:29:46 +0530

    Update Adhar.jsx

--- src/components/Adhar.jsx
+++ src/components/Adhar.jsx
@@ -12,7 +12,7 @@ const Adhar = () => {
   };
 
   const fetchData = async (input) => {
-    if (input.trim() === "" || input.length < 12) {
+    if (input.trim() === "" || input.length < 12 || input.length>12) {
       toast.error("Please recheck your Aadhaar number 😢", {
         position: "bottom-left",
         autoClose: 5000,
@@ -43,7 +43,7 @@ const Adhar = () => {
       console.log(response.data.data);
       setResult(response.data.data);
       if (response.data.data === true) {
-        toast.success("Aadhaar is verified 😊", {
+        toast.success("Aadhaar number exist 😊", {
           position: "bottom-left",
           autoClose: 5000,
           hideProgressBar: false,
@@ -53,7 +53,7 @@ const Adhar = () => {
           progress: undefined,
           theme: "dark",
         });
-      } else if (response.daata.data === false) {
+      } else if (response.data.data === false) {
         toast.error("Aadhaar doesn't exist 😢", {
           position: "bottom-left",
           autoClose: 5000,
@@ -92,7 +92,7 @@ const Adhar = () => {
       <div className="mt-20 flex flex-col justify-center items-center">
         <img className="mb-3" src="https://imgs.search.brave.com/yZ6dcrM3ZwgoMtXnljj-rfpCFf3V01-5h8zO4xykrno/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91aWRh/aS5nb3YuaW4vaW1h/Z2VzL2xhbmdQYWdl/L1BhZ2UtMS5zdmc.svg" alt="" />
         <p className="text-3xl font-extrabold text-yellow-500">
-          Verify your Aadhaar
+          Verify Your Aadhaar ID
         </p>
         <form onSubmit={submitHandler} className="flex flex-col md:flex-row space-x-3 w-8/12 md:w-6/12">
           <input
@@ -101,6 +101,7 @@ const Adhar = () => {
             className="bg-[#202123] rounded-3xl text-white px-4 py-4 w-12/12 md:w-11/12 my-8"
             type="text"
             placeholder="Enter Your Adhaar number"
+            maxlength={12}
 
           />
           <button onClick={submitHandler} className="self-center w-20">
@@ -115,4 +116,4 @@ const Adhar = () => {
   );
 };
 
-export default Adhar;
+export default Adhar;
