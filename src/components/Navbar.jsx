7ef37f2c003760631f38a5b30a610eba8f44419e
Bishwo Bista <bishwo5bista@gmail.com>
2023-10-01 14:23:10 +0545

    add: logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,19 +12,7 @@ const Navbar = () => {
             className="flex title-font font-medium items-center text-white mb-4 md:mb-0 cursor-pointer"
           // Added cursor-pointer class here
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-            </svg>
-            <span className="ml-3 text-xl">VeriIDy</span>
+            <img src="/logo.png" alt="" className="w-auto h-14" />
           </a>
           <nav className="hidden md:block md:ml-auto flex flex-wrap items-center text-base justify-center">
             <a className="mr-5 hover:text-white cursor-pointer">Home</a>
